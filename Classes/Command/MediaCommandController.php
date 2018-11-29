neos/media

commit f3a15ae4d34cd0c796f384e4e85b8280add6171a

FEATURE: Add limit flaf to media:removeunused
This adds a flog --limit to limit the result of unused
assets displayed and removed for a single run.

Classes/Command/MediaCommandController.php
@@ -149,9 +149,10 @@ class MediaCommandController extends CommandController
      * @param bool $quiet If set, only errors will be displayed.
      * @param bool $assumeYes If set, "yes" is assumed for the "shall I remove ..." dialogs
      * @param string $onlyTags Comma-separated list of asset tags, that should be taken into account
+     * @param int $limit Limit the result of unused assets displayed and removed for this run.
      * @return void
      */
-    public function removeUnusedCommand(string $assetSource = '', bool $quiet = false, bool $assumeYes = false, string $onlyTags = '')
+    public function removeUnusedCommand(string $assetSource = '', bool $quiet = false, bool $assumeYes = false, string $onlyTags = '', int $limit = 0)
     {
         $iterator = $this->assetRepository->findAllIterator();
         $assetCount = $this->assetRepository->countAll();
@@ -181,6 +182,10 @@ class MediaCommandController extends CommandController
         foreach ($this->assetRepository->iterate($iterator) as $asset) {
             !$quiet && $this->output->progressAdvance(1);
 
+            if($unusedAssetCount === $limit) {
+                break;
+            }
+
             if (!($asset instanceof Asset)) {
                 continue;
             }
@@ -193,7 +198,6 @@ class MediaCommandController extends CommandController
             if (trim($onlyTags) && !($assetTagsMatchFilterTags($asset->getTags(), $onlyTags))) {
                 continue;
             }
-
             if ($asset->getUsageCount() !== 0) {
                 continue;
             }

commit b1bb57d424dc0dcd39821b9d262c8614394f675e

BUGFIX: media:removeunused only works with limit flag
This change fixes a regression introduced in
f3a15ae4d34cd0c796f384e4e85b8280add6171a where a new command
line flag "--limit" was introduced. The command did not detect
any unused assets unless the --limit flag was specified,
because the iteration would stop right away.

Classes/Command/MediaCommandController.php
@@ -152,7 +152,7 @@ class MediaCommandController extends CommandController
      * @param int $limit Limit the result of unused assets displayed and removed for this run.
      * @return void
      */
-    public function removeUnusedCommand(string $assetSource = '', bool $quiet = false, bool $assumeYes = false, string $onlyTags = '', int $limit = 0)
+    public function removeUnusedCommand(string $assetSource = '', bool $quiet = false, bool $assumeYes = false, string $onlyTags = '', int $limit = null)
     {
         $iterator = $this->assetRepository->findAllIterator();
         $assetCount = $this->assetRepository->countAll();
@@ -163,7 +163,7 @@ class MediaCommandController extends CommandController
 
         $filterByAssetSourceIdentifier = $assetSource;
         if ($filterByAssetSourceIdentifier === '') {
-            !$quiet && $this->outputLine('<b>Searching for unused assets:</b>');
+            !$quiet && $this->outputLine('<b>Searching for unused assets in all asset sources:</b>');
         } else {
             !$quiet && $this->outputLine('<b>Searching for unused assets of asset source "%s":</b>', [$filterByAssetSourceIdentifier]);
         }
@@ -183,7 +183,7 @@ class MediaCommandController extends CommandController
         foreach ($this->assetRepository->iterate($iterator) as $asset) {
             !$quiet && $this->output->progressAdvance(1);
 
-            if ($unusedAssetCount === $limit) {
+            if ($limit !== null && $unusedAssetCount === $limit) {
                 break;
             }
 